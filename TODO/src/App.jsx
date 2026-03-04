03db7fe6522b4faf826af1e9cf0bfd8a38b29c72
Tushar <tusharjana.aj3@gmail.com>
2026-03-05 00:16:05 +0530

    ToDo added localSTorage and Check

--- TODO/src/App.jsx
+++ TODO/src/App.jsx
@@ -1,29 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 function App() {
   const [task, setTask] = useState("");
   const [list, setList] = useState([]);
-  const [check,setCheck]=useState(true)
+  const [check, setCheck] = useState(true);
   console.log(check);
-  
+
   console.log(task);
 
+  // function addData() {
+  //   setList([...list, task]);
+  //   setTask("");
+
+  // }
+
   function addData() {
-    setList([...list, task]);
+    const updatedList = [...list, task];
+    setList(updatedList);
+    saveData(updatedList);
     setTask("");
-    
   }
+  function loadData() {
+    const savedList = localStorage.getItem("todos");
+    if (savedList) {
+      setList(JSON.parse(savedList));
+    }
+  }
+
+  function saveData(data) {
+    localStorage.setItem("todos", JSON.stringify(data));
+  }
+  useEffect(() => {
+    loadData();
+  }, []);
 
   // console.log(list);
 
-  const handleDelete = (val) => {
-    // console.log(list);
-    console.log(val);
-    const updatedList=list.filter(x=> x!==val)
-    
-    setList(updatedList)
-  };
+  // const handleDelete = (val) => {
+  //   // console.log(list);
+  //   console.log(val);
+  //   const updatedList=list.filter(x=> x!==val)
 
+  //   setList(updatedList)
+  // };
+  const handleDelete = (val) => {
+    const updatedList = list.filter((x) => x !== val);
+    setList(updatedList);
+    saveData(updatedList);
+  };
   return (
     <div className="container">
       <div className="todo">
@@ -47,10 +71,16 @@ function App() {
       <div>
         <ul className="tast-list">
           {list.map((val, idx) => (
-            <li key={idx} className="task-item" onClick={()=>!setCheck}>
-              <span >{val}</span>
+            <li
+              key={idx}
+              className="task-item"
+              onClick={() => setCheck(!check)}
+            >
+              <span style={{ textDecoration: check ? "none" : "line-through" }}>
+                {val}
+              </span>
               <button className="delete-btn" onClick={() => handleDelete(val)}>
-                × 
+                ×
               </button>
             </li>
           ))}
