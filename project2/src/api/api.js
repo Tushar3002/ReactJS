094829f388205f63c3ba1ce7a63fdb08552f59b8
tushar jana <145481506+Tushar3002@users.noreply.github.com>
2026-05-01 17:53:10 +0530

    added the interceptor for error and Toast

--- project2/src/api/api.js
+++ project2/src/api/api.js
@@ -4,8 +4,6 @@ export const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-
-
 // axios.get("/api/data", {
 //   headers: { Authorization: `Bearer ${token}` }
 // });
@@ -20,11 +18,56 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+// import { toast } from "sonner";
+import { toast } from "react-toastify";
 
+api.interceptors.response.use(
+  (response) => {
+    // Show success toast only when needed
+    if (response.config?.showSuccessToast) {
+      const message =
+        response.config.successMessage ||
+        response.data?.message ||
+        "Action successful";
 
+      toast.success(message, {
+        theme: "colored",
+      });
+    }
+
+    return response;
+  },
+
+  (error) => {
+    const message = error.response?.data?.message || "Something went wrong";
+
+    const status = error.response?.status;
+
+    // Avoid toast if explicitly disabled
+    if (!error.config?.skipToast && status !== 401) {
+      toast.error(message, {
+        theme: "colored",
+      });
+    }
+
+    // Unauthorized
+    if (status === 401) {
+      localStorage.removeItem("token");
+    } 
+ 
+    //  Forbidden
+    if (status === 403) {
+      toast.error(message, {
+        theme: "colored",
+      });
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export const getProducts = () => {
-  return api.get("/products");
+  return api.get("/products", { skipToast: true });
 };
 
 export const getSingleProducts = (id) => {
@@ -32,41 +75,43 @@ export const getSingleProducts = (id) => {
 };
 
 export const createProduct = (data) => {
-  return api.post("/products", data);
+  return api.post("/products", data, { showSuccessToast: true });
 };
 
-export const deleteProduct=(id)=>{
-  return api.delete(`/products/${id}`)
-}
+export const deleteProduct = (id) => {
+  return api.delete(`/products/${id}`, { showSuccessToast: true });
+};
 
-export const editProduct=(id,data)=>{
-  return api.put(`/products/${id}`,data)
-}
+export const editProduct = (id, data) => {
+  return api.put(`/products/${id}`, data);
+};
 
-export const loginUser = (data) => api.post("/auth/login", data);
-export const registerUser = (data) => api.post("/auth/register", data);
+export const loginUser = (data) =>
+  api.post("/auth/login", data, { showSuccessToast: true }); //, successMessage: "Login successful 🎉"
+export const registerUser = (data) =>
+  api.post("/auth/register", data, { showSuccessToast: true });
 
-export const createOrder = (data) => api.post("/orders", data);
-export const getOrders=(data)=>{
-  return api.get("/orders")
-}
+export const createOrder = (data) =>
+  api.post("/orders", data, { showSuccessToast: true });
+export const getOrders = (data) => {
+  return api.get("/orders", { skipToast: true });
+};
 
-export const getUser = ()=>{
-  return api.get('/auth/me')
-}
+export const getUser = () => {
+  return api.get("/auth/me");
+};
 
-export const updateUserRole = (id,data)=>{
-  return api.put(`/admin/updateUser/${id}`,data)
-}
+export const updateUserRole = (id, data) => {
+  return api.put(`/admin/updateUser/${id}`, data, { showSuccessToast: true });
+};
 
-export const getAllUser = ()=>{
-  return api.get('/admin/allUsers')
-}
+export const getAllUser = () => {
+  return api.get("/admin/allUsers");
+};
 
-export const deleteUser = (userId)=>{
-  return api.delete(`/admin/deleteUser/${userId}`)
-}
-
+export const deleteUser = (userId) => {
+  return api.delete(`/admin/deleteUser/${userId}`, { showSuccessToast: true });
+};
 
 export const getDashboardStats = () => {
   return api.get("/admin/dashboard");
@@ -74,15 +119,16 @@ export const getDashboardStats = () => {
 
 //wishList
 
-export const addWish=(productId)=>{
-  return api.post("/wishlist",{productId})
-}
+export const addWish = (productId) => {
+  console.warn("ADD WISHLIST : ",productId);
+  
+  return api.post("/wishlist", { productId }, { showSuccessToast: true },);
+};
 
-export const getWish=()=>{
-  return api.get("/wishlist")
-}
+export const getWish = () => {
+  return api.get("/wishlist",{ skipToast: true },);
+};
 
-export const removeWish=(productId)=>{
-  return api.delete(`/wishlist/${productId}`)
-}
-
+export const removeWish = (productId) => {
+  return api.delete(`/wishlist/${productId}`, { showSuccessToast: true });
+};
